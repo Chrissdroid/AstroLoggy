b69eb3018c6edd068dae1f7199ca2d2b2aaaf28b
Astroalex <alexis31.balthazard@gmail.com>
2023-06-14 10:17:56 +0200

    refactor and outdated code clearing

--- src/scripts/i18n.ts
+++ src/scripts/i18n.ts
@@ -1,12 +1,13 @@
+import { GetStaticPaths } from 'astro';
 import { readFileSync } from 'fs'
-import json5 from 'json5'
+import * as json5 from 'json5'
 
 const languages = [
 	'en',
 	'fr'
 ].sort((a, b) => a.localeCompare(b))
 
-export function createI18n(slug: string, langs = languages) {
+export function createI18n(slug: string, langs = languages): GetStaticPaths {
 	return async () => {
 		return await Promise.all(langs.map(async lang => {
 			const file = readFileSync(`./src/i18n/${slug}/${lang}.json5`).toString()
